21673a6f1c6dbb412f8753ab7291bfa54348814c
alec dye <alecdye@gmail.com>
2020-08-07 10:16:28 -0700

    Complete ex 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,30 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+
+"""
+where the `source` string represents the starting airport and the
+`destination` string represents the next airport along our trip. The
+ticket for your first flight has a destination with a `source` of
+`NONE`, and the ticket for your final flight has a `source` with a
+`destination` of `NONE`. 
+
+tickets = [
+    Ticket{ source: "PIT", destination: "ORD" },
+    Ticket{ source: "XNA", destination: "CID" },
+    Ticket{ source: "SFO", destination: "BHM" },
+    Ticket{ source: "FLG", destination: "XNA" },
+    Ticket{ source: "NONE", destination: "LAX" },
+    Ticket{ source: "LAX", destination: "SFO" },
+    Ticket{ source: "CID", destination: "SLC" },
+    Ticket{ source: "ORD", destination: "NONE" },
+    Ticket{ source: "SLC", destination: "PIT" },
+    Ticket{ source: "BHM", destination: "FLG" }
+]
+
+output:
+["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+"""
+
+# tickets refer to an array of these
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -6,9 +32,19 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    route = [None] * length
+
+    # loop Ticket class entries into cache
+    for ticket in tickets:
+        # Ticket.source(key) Ticket.destination(item)
+        cache[ticket.source] = ticket.destination
+
+    # specifically looking for "NONE"
+    destination = cache["NONE"]
+
+    for flight in range(length):
+        route[flight] = destination
+        destination = cache[destination]
 
     return route
